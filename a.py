e8a435f0f38307103f8d19b9d2ab4111872c1962
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-13 23:45:49 +0530

    Add dot prod

--- a.py
+++ a.py
@@ -1,15 +1,5 @@
 import numpy as np
 
-X = np.array([
-    [
-        [1, 2],
-        [3, 4]
-    ],
-    [
-        [5, 6],
-        [7, 8]
-    ]
-])
-
-print(X.ndim)
-# 3
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+print(a * b) # dot product
