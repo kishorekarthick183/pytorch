d29abc656d61efe85926d8a6964bbd1124e31dda
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-13 23:50:44 +0530

    Add reshape

--- a.py
+++ a.py
@@ -1,17 +1,6 @@
 import numpy as np
-
-# a = np.array([1, 2, 3])
-
-# print(a.shape) # 3
-# print(a + 10)
-
-X = np.array([
-    [1, 2, 3],
-    [4, 5, 6]
-])
-
-b = np.array([10, 20, 30])
-
-print(X + b)
-# [[11 22 33]
-#  [14 25 36]]
+x = np.array([1, 2, 3, 4, 5, 6])
+x = x.reshape(2, 3)
+print(x)
+# [[1 2 3]
+# [4 5 6]]
