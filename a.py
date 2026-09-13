3a671b3fa7a85ea0efb128f2b099c80a322920de
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-13 23:10:49 +0530

    Read contents

--- a.py
+++ a.py
@@ -1,16 +1,3 @@
-def logger(func):
-
-    def wrapper():
-        print("Function started")
-
-        func()
-
-        print("Function finished")
-
-    return wrapper
-
-@logger
-def hello():
-    print("Hello")
-
-hello()
+with open("data.txt") as file: 
+    content = file.read()
+    print(content)
