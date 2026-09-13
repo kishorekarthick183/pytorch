7f28e541c04e332af9d53d10a14e385347408576
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-13 23:09:25 +0530

    No logger wip

--- a.py
+++ a.py
@@ -1,18 +1,17 @@
-class Student:
-    credits = 3
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+def logger(func):
 
-    def introduce(self):
-        print(f"My name is {self.name}")
-        print(f"I am {self.age} years old")
+    def wrapper():
+        print("Function started")
 
-s1 = Student("Kishore", 22)
-s2 = Student("Arun", 21)
+        func()
 
-s1.introduce()
-s2.introduce()
-print(s1.credits)
-print(s2.credits)
-# credits is class based not instance. both student have same credit
+        print("Function finished")
+
+    return wrapper
+
+def hello():
+    print("Hello")
+
+hello = logger(hello)
+
+hello()
