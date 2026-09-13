009fdc3485de9d61ab61a40e3986eba30df141db
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-13 23:52:10 +0530

    -1 in reshape

--- a.py
+++ a.py
@@ -1,6 +1,6 @@
 import numpy as np
 x = np.array([1, 2, 3, 4, 5, 6])
-x = x.reshape(2, 3)
-print(x)
-# [[1 2 3]
-# [4 5 6]]
+new_x = x.reshape(2, -1)
+new_y = x.reshape(3, -1)
+# print(new_x)
+print(new_y)
