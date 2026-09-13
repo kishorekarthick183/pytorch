69bd5ee38778cbb4e8bd3e86923bd9cd52fdd3e5
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-13 23:26:06 +0530

    Add example for class

--- a.py
+++ a.py
@@ -1,14 +1,24 @@
-def numbers():
-    print("Before 1")
-    yield 1
+class BankAccount: 
+    def __init__(self, owner: str, balance: float = 0.0):
+        self.owner = owner
+        self.balance = balance
+    
+    def deposit(self, amount: float) -> float: 
+        if amount <= 0:
+            raise ValueError("deposit amt must be positive")
+        self.balance += amount
+        print(f"Deposited ${amount: .2f}. New Balance: ${self.balance: .2f}")
+        return self.balance 
+    def withdraw(self, amount: float) -> float: 
+        if amount <= 0: 
+            raise ValueError("deposit amt must be positive")
+        if self.balance < amount: 
+            raise ValueError("insufficient funds")
+        
+        self.balance  -= amount
+        print(f"Withdrew ${amount:.2f}. New balance: ${self.balance:.2f}")
+        return self.balance
 
-    print("Before 2")
-    yield 2
-
-    print("Before 3")
-    yield 3
-
-g = numbers()
-print(g)
-print(next(g))
-print(next(g))
+acc = BankAccount("Kishore", 100.0)
+acc.deposit(50.0)
+acc.withdraw(30.0)
