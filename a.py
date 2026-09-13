3f9889bdd1120327268394a9b366b5a24bb721ce
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-13 23:55:32 +0530

    arrange & (@ vs *)

--- a.py
+++ a.py
@@ -1,11 +1,12 @@
 import numpy as np
 
-X = np.array([
-    [1, 2, 3],
-    [4, 5, 6]
-])
+# @ vs * 
+# @ matrix multiplction
+# * element multiplication
 
-# val = X.flatten()
-# print(val)
-
-print(X.T)
+a = np.arange(0, 10)
+print(a)
+b = np.zeros((2, 3))
+print(b)
+c = np.ones((2, 3))
+print(c)
