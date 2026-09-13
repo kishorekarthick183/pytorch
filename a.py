d3f1262ab4b8b911a7cc4a325ef9c7b542682ff3
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-13 23:53:19 +0530

    flatten && transpose

--- a.py
+++ a.py
@@ -1,6 +1,11 @@
 import numpy as np
-x = np.array([1, 2, 3, 4, 5, 6])
-new_x = x.reshape(2, -1)
-new_y = x.reshape(3, -1)
-# print(new_x)
-print(new_y)
+
+X = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+# val = X.flatten()
+# print(val)
+
+print(X.T)
