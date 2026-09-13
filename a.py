9ec870c5687db5f72e35c75c6430cb0a178461b7
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-14 00:26:02 +0530

    torch.no_grad

--- a.py
+++ a.py
@@ -1,9 +1,14 @@
 import torch
 
-x = torch.tensor(3.0, requires_grad=True)
+# Dummy model weights
+w = torch.tensor([1.5], requires_grad=True)
+b = torch.tensor([0.5], requires_grad=True)
 
-y = x ** 2
+test_x = torch.tensor([2.0, 4.0, 6.0])
 
-y.backward()
+# Disable gradient tracking for inference
+with torch.no_grad():
+    predictions = test_x * w + b
 
-print(x.grad)
+print(predictions)
+print(predictions.requires_grad)  # Output: False
