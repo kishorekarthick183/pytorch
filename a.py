b7c1a58208b2b29faef621121a1d82ebf4be83b8
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-13 23:48:07 +0530

    Add broadcast

--- a.py
+++ a.py
@@ -1,5 +1,17 @@
 import numpy as np
 
-a = np.array([1, 2, 3])
-b = np.array([4, 5, 6])
-print(a * b) # dot product
+# a = np.array([1, 2, 3])
+
+# print(a.shape) # 3
+# print(a + 10)
+
+X = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
+
+b = np.array([10, 20, 30])
+
+print(X + b)
+# [[11 22 33]
+#  [14 25 36]]
