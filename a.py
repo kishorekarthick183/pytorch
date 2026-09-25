24263b0c71ef091bc3c83bad97ed56672a975099
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-25 21:52:46 +0530

    complete linear regression

--- a.py
+++ a.py
@@ -21,38 +21,27 @@ b = torch.tensor(0.0, requires_grad=True)
 
 learning_rate = 0.01
 
-for epoch in range(1):
+w = torch.tensor(0.0, requires_grad=True)
+b = torch.tensor(0.0, requires_grad=True)
 
-    # Forward
+for epoch in range(100):
+
     prediction = w * X + b
 
-    # Loss
     loss = ((prediction - y) ** 2).mean()
 
-    # Backward
     loss.backward()
 
-    print("prediction:\n", prediction)
-    print("loss:", loss.item())
-    print("w gradient:", w.grad.item())
-    print("b gradient:", b.grad.item())
-
-    # Update
     with torch.no_grad():
         w -= learning_rate * w.grad
         b -= learning_rate * b.grad
 
-    # Reset gradients
     w.grad.zero_()
     b.grad.zero_()
 
-    # Print every 100 epochs
-    if epoch % 100 == 0:
-        print(
-            f"epoch={epoch}, "
-            f"loss={loss.item():.4f}, "
-            f"w={w.item():.4f}, "
-            f"b={b.item():.4f}"
-        )
+new_X = torch.tensor([[6.0]])
 
+prediction = w * new_X + b
+
+print(prediction)
 # explain the process of chaining the loss function wrt w and b ? 
