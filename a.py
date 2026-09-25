5ce8849fb92668aaf4cf8f5603591999297f1d02
Kishore Karthick <kishorekarthick183@gmail.com>
2026-09-25 21:47:05 +0530

    differentiation explain by backpropagation

--- a.py
+++ a.py
@@ -1,30 +1,58 @@
 import torch
 
+X = torch.tensor([
+    [1.0],
+    [2.0],
+    [3.0],
+    [4.0],
+    [5.0]
+])
+
+y = torch.tensor([
+    [3.0],
+    [5.0],
+    [7.0],
+    [9.0],
+    [11.0]
+])
+
 w = torch.tensor(0.0, requires_grad=True)
 b = torch.tensor(0.0, requires_grad=True)
-x = torch.tensor(3.0)
-
-actual = torch.tensor(7.0)
 
 learning_rate = 0.01
-prediction = w * x + b
-print("prediction ", prediction)
 
-# Loss
-loss = (prediction - actual) ** 2
+for epoch in range(1):
 
-print(loss) # (7 - (0 * 3 + 0))^2 -> 49.
+    # Forward
+    prediction = w * X + b
 
-# Gradient
-loss.backward()
+    # Loss
+    loss = ((prediction - y) ** 2).mean()
 
-print(w.grad) # -42
-print(b.grad) # -14
+    # Backward
+    loss.backward()
 
-# Update
-with torch.no_grad():
-    w -= learning_rate * w.grad
-    b -= learning_rate * b.grad
-    print(w) # 0.42
-    print(b) # 0.14
-    print("prediction new ", w * x+ b)
+    print("prediction:\n", prediction)
+    print("loss:", loss.item())
+    print("w gradient:", w.grad.item())
+    print("b gradient:", b.grad.item())
+
+    # Update
+    with torch.no_grad():
+        w -= learning_rate * w.grad
+        b -= learning_rate * b.grad
+
+    # Reset gradients
+    w.grad.zero_()
+    b.grad.zero_()
+
+    # Print every 100 epochs
+    if epoch % 100 == 0:
+        print(
+            f"epoch={epoch}, "
+            f"loss={loss.item():.4f}, "
+            f"w={w.item():.4f}, "
+            f"b={b.item():.4f}"
+        )
+
+# explain the process of chaining the loss function wrt w and b ? 
